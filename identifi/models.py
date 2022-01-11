7c4993040fc5b67faadf56e2f5c77ad1ae259d6a
Caesium24 <esh2424@gmail.com>
2022-01-11 15:54:26 -0800

    Added initial db models for User and Income.

--- identifi/models.py
+++ identifi/models.py
@@ -5,4 +5,12 @@ db = SQLAlchemy()
 class User(db.model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.string(100), nullable=False)
-    
+
+class Income(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    income_name =
+    income_type =
+    income_schedule =
+    income_dependants =
+    income_deductions =
+    income_net =
