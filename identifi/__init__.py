4d8103b625e671977980a97ec9e57a4dab97429d
Caesium24 <esh2424@gmail.com>
2022-01-19 08:26:41 -0800

    Updated application structure

--- identifi/__init__.py
+++ identifi/__init__.py
@@ -1,26 +1,5 @@
 import os
-from flask import Flask, redirect, render_template, url_for
-
-def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY=os.environ.get('SECRET_KEY')
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-    
-    from .models import db
-    db.init_app(app)
-
-    @app.route('/')
-    def index():
-        return redirect(url_for('identifi'))
-    
-    @app.route('/identifi')
-    def identifi():
-
-    
-    return app
+from identifi import app
+   
+if __name__ == '__main__':
+    app.run(debug=True)
