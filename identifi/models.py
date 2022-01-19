3656edaf6e8c196bdf025b545f8bb7b0a0f6b56a
Caesium24 <esh2424@gmail.com>
2022-01-19 08:27:09 -0800

    added expense and debt models

--- identifi/models.py
+++ identifi/models.py
@@ -6,12 +6,32 @@ class User(db.model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False)
+    password =
 
 class Income(db.model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id =
     income_name = db.Column(db.String(50), nullable=False)
     income_type = db.Column(db.String(50), nullable=False)
     income_schedule = db.Column(db.String(50), nullable=False)
     income_dependants = db.Column(db.Float, nullable=True)
     income_deductions = db.Column(db.Float, nullable=True)
     income_net = db.Column(db.Float, nullable=True)
+
+class Expense(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id =
+    exp_name = db.Column(db.String(50), nullable=False)
+    exp_type = db.Column(db.String(50), nullable=False)
+    exp_schedule = db.Column(db.String(50), nullable=False)
+    exp_payment = db.Column(db.Float, nullable=True)
+
+class Debt(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id =
+    debt_name = db.Column(db.String(50), nullable=False)
+    debt_type = db.Column(db.String(50), nullable=False)
+    debt_schedule = db.Column(db.String(50), nullable=False)
+    debt_interest = db.Column(db.Float, nullable=True)
+    debt_total = db.Column(db.Float, nullable=True)
+    debt_payment = db.Column(db.Float, nullable=True)
